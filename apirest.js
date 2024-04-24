3a30ac958b9610b6c55bc576ee084de41b9ba954
JadirMiramontes <jadirescuela23@gmail.com>
2024-04-24 09:57:56 -0600

    Se agrego el configJS, rotesJS y pruebas con thunder client

--- apirest.js
+++ apirest.js
@@ -1,18 +1,22 @@
-var express = require('express');
-var app = express();
+//Require packages and set the port
+const express = require('express');
+const port = 3002;
+//para permitir manejo de POST y PUT
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const app = express();
 
-app.get('/',function(req,res){
-    res.send('Web inicial de mi API');
-});
+//Usar Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
 
-app.get('/saludo', function(req,res){
-    res.send('Hola mundo');
-});
+routes(app);
 
-app.get('/despedida',function(req,res){
-    res.send('Adios mundo cruel');
-});
+//Iniciar el servidor 
+const server = app.listen(port, (error)=>{
+    if(error) return console.log(`Error:${error}`);
 
-app.listen(3000, function(){
-    console.log('Aplicacion ejemplo de API, escuchando en el puerto 3000')
+    console.log(`El servidor escucha en el puerto ${server.address().port}`);
 });
